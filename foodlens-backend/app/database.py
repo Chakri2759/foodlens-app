d75594e934deb421176c3b614bf0da11d5a003ab
Chakri Ramisetti <chakriramisetti555@gmail.com>
2026-04-05 09:44:56 +0530

    .env added

--- foodlens-backend/app/database.py
+++ foodlens-backend/app/database.py
@@ -1,14 +1,16 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
-# ✅ Read Mongo URL from environment
-MONGO_URL = "mongodb+srv://chakriramisetti555_db_user:ylccNmTKk4jiq0qe@userdata.jtivg09.mongodb.net/foodlens?retryWrites=true&w=majority"
+# Load env variables
+load_dotenv()
+
+# Read from .env
+MONGO_URL = os.getenv("MONGO_URL")
 
 if not MONGO_URL:
     raise ValueError("MONGO_URL is not set")
 
-# ✅ Create client
 client = AsyncIOMotorClient(MONGO_URL)
 
-# ✅ Database reference
 db = client.foodlens
